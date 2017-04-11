bef717b17a00077a168b9d85973de162a6c6b4ba
xhuang17 <xhuang17@ncsu.edu>
2017-04-11 00:09:09 -0400

    Update multi_node.sh

--- multi_node.sh
+++ multi_node.sh
@@ -1,13 +1,16 @@
+# clean up
+
+# network configuration
+
 
 # packstack installation
-
 # set language configuration
 sed -i 's/LANG=en_US.utf-8/#LANG=en_US.utf-8/g' /etc/environment
 sed -i 's/LC_ALL=en_US.utf-8/#LC_ALL=en_US.utf-8/g' /etc/environment
 echo "LANG=en_US.utf-8" >> /etc/environment
 echo "LC_ALL=en_US.utf-8" >> /etc/environment
 
-# install packstack
+# install packstack from the answer file
 # change the file name
 sudo yum install -y centos-release-openstack-newton
 sudo yum update -y
