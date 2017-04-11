77e704dc16d5cf75f234a4a12f305c34edcb1510
xhuang17 <xhuang17@ncsu.edu>
2017-04-11 00:05:28 -0400

    packstack install

---
+++ multi_node.sh
@@ -0,0 +1,15 @@
+
+# packstack installation
+
+# set language configuration
+sed -i 's/LANG=en_US.utf-8/#LANG=en_US.utf-8/g' /etc/environment
+sed -i 's/LC_ALL=en_US.utf-8/#LC_ALL=en_US.utf-8/g' /etc/environment
+echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8" >> /etc/environment
+
+# install packstack
+# change the file name
+sudo yum install -y centos-release-openstack-ocata
+sudo yum update -y
+sudo yum install -y openstack-packstack
+sudo packstack --answer-file=FILE
