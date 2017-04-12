8e8a678377dc35c729c1202a533085d755e5c59a
htan5 <htan5@ncsu.edu>
2017-04-12 18:13:40 -0400

    change precondition

--- script/precondition.sh
+++ script/precondition.sh
@@ -18,9 +18,9 @@ function before_install()
 	yum install lsof;
 	yum install ebtables ipset;
 	#clean up port
-	lsof -i :5000|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
-	lsof -i :35357|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
-	lsof -i :11211|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :5000|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :35357|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :11211|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
 	lsof -i :9696|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
 
 	#Glance
@@ -28,17 +28,17 @@ function before_install()
 	/sbin/service mysqld start;
 	yum install lsof;
 	#clean up port
-	lsof -i :5000|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
-	lsof -i :35357|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
-	lsof -i :11211|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :5000|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :35357|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :11211|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
 	lsof -i :9292|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
 
 	#Nova
 	yum install lsof;
 	#clean up port
-	lsof -i :5000|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
-	lsof -i :35357|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
-	lsof -i :11211|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :5000|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :35357|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
+	# lsof -i :11211|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
 	lsof -i :8774|grep -v "PID"|awk '{print "kill -9",$2}'|sh;
 
 }
