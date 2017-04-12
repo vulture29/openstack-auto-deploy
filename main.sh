d659d1ddffd8ddf0ede46ccf0fb8b784e4967ea1
xhuang17 <xhuang17@ncsu.edu>
2017-04-12 16:22:09 -0400

    input

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 USER_INPUT=1;
-CUSTOMIZED_CONFIG_PATH="/Users/macbook/Desktop/flow.jpeg"
+CUSTOMIZED_CONFIG_PATH="/Users/macbook/Desktop/answer_customized.txt"
+DEFAULT_CONFIG_PATH="/Users/macbook/Desktop/answer_default.txt"
 echo "CUSTOMIZED_CONFIG_PATH is " $CUSTOMIZED_CONFIG_PATH
 while read -p "Want to proceed with default config -- allinone?(yes/no) " REPLY1 ; do
   case $REPLY1 in
@@ -21,7 +22,7 @@ while read -p "Want to proceed with default config -- allinone?(yes/no) " REPLY1
 
 				break;;
 				no)	break;;
-				*) "Please enter (yes) or (no).";;
+				*) echo "Please enter (yes) or (no).";;
 			esac
 		done
 	else
@@ -30,6 +31,77 @@ while read -p "Want to proceed with default config -- allinone?(yes/no) " REPLY1
 	if [ $USER_INPUT -eq 1 ]; then
 			echo "Starting user input configuration..."
 			# user input configuration
+			read -p "Please enter the network node IP: " NETWORK_IP
+			read -p "Please enter the conpute node IP: " COMPUTE_IP
+			read -p "Please enter the controller node IP: " CONTROLLER_IP
+			read -p "Please enter the storage node IP: " STORAGE_IP
+			while read -p "Do you want to enable NOVA?(y/n)" NOVA_ENABLE ; do
+				case $NOVA_ENABLE in
+					y)	break;;
+					n)	break;;
+					*)	echo "Please enter y/n.";;
+				esac
+			done
+			while read -p "Do you want to enable NEUTREN?(y/n)" NEUTREN_ENABLE ; do
+				case $NEUTREN_ENABLE in
+					y)	break;;
+					n)	break;;
+					*)	echo "Please enter y/n.";;
+				esac
+			done
+			while read -p "Do you want to enable GLANCE?(y/n)" GLANCE_ENABLE ; do
+				case $GLANCE_ENABLE in
+					y)	break;;
+					n)	break;;
+					*)	echo "Please enter y/n.";;
+				esac
+			done
+			while read -p "Do you want to enable HORIZON?(y/n)" HORIZON_ENABLE ; do
+				case $HORIZON_ENABLE in
+					y)	break;;
+					n)	break;;
+					*)	echo "Please enter y/n.";;
+				esac
+			done
+			while read -p "Do you want to enable CINDER?(y/n)" CINDER_ENABLE ; do
+				case $CINDER_ENABLE in
+					y)	break;;
+					n)	break;;
+					*)	echo "Please enter y/n.";;
+				esac
+			done
+			while read -p "Do you want to enable SWIFT?(y/n)" SWIFT_ENABLE ; do
+				case $SWIFT_ENABLE in
+					y)	break;;
+					n)	break;;
+					*)	echo "Please enter y/n.";;
+				esac
+			done
+
+			# cp $DEFAULT_CONFIG_PATH $DEFAULT_CONFIG_PATH".bkg"
+			sed -i 's/^CONFIG_CONTROLLER_HOST/#CONFIG_CONTROLLER_HOST/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_CONTROLLER_HOST = " $COMPUTE_IP >> $DEFAULT_CONFIG_PATH
+			sed -i 's/^CONFIG_NETWORK_HOST/#CONFIG_NETWORK_HOST/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_NETWORK_HOST = " $NETWORK_IP >> $DEFAULT_CONFIG_PATH
+			sed -i 's/^CONFIG_COMPUTE_HOST/#CONFIG_COMPUTE_HOST/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_COMPUTE_HOST = " $COMPUTE_IP >> $DEFAULT_CONFIG_PATH
+			sed -i 's/^CONFIG_STORAGE_HOST/#CONFIG_STORAGE_HOST/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_STORAGE_HOST = " $STORAGE_IP >> $DEFAULT_CONFIG_PATH
+
+			sed -i 's/^CONFIG_NOVA_INSTALL/#CONFIG_NOVA_INSTALL/g' "$DEFAULT_CONFIG_PATH"
+			echo "CONFIG_NOVA_INSTALL = " $NOVA_ENABLE >> $DEFAULT_CONFIG_PATH
+			sed -i 's/^CONFIG_NEUTREN_INSTALL/#CONFIG_NEUTREN_INSTALL/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_NEUTREN_INSTALL = " $NEUTREN_ENABLE >> $DEFAULT_CONFIG_PATH
+			sed -i 's/^CONFIG_GLANCE_INSTALL/#CONFIG_GLANCE_INSTALL/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_GLANCE_INSTALL = " $GLANCE_ENABLE >> $DEFAULT_CONFIG_PATH
+			sed -i 's/^CONFIG_HORIZON_INSTALL/#CONFIG_HORIZON_INSTALL/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_HORIZON_INSTALL = " $HORIZON_ENABLE >> $DEFAULT_CONFIG_PATH
+			sed -i 's/&CONFIG_CINDER_INSTALL/#CONFIG_CINDER_INSTALL/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_CINDER_INSTALL = " $CINDER_ENABLE >> $DEFAULT_CONFIG_PATH
+			sed -i 's/^CONFIG_SWIFT_INSTALL/#CONFIG_SWIFT_INSTALL/g' $DEFAULT_CONFIG_PATH
+			echo "CONFIG_SWIFT_INSTALL = " $SWIFT_ENABLE >> $DEFAULT_CONFIG_PATH
+
+			# install with the default config file
 
 	fi
 	break;;
