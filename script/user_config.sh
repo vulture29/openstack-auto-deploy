178a8743efd9dfe7cc36041cd54d36c43e445d3d
jsingh8 <jsingh8@ncsu.edu>
2017-04-15 18:17:34 -0400

    Formatted

--- script/user_config.sh
+++ script/user_config.sh
@@ -1,83 +1,130 @@
 #!/bin/bash
 
 USER_INPUT=1;
+
 CUSTOMIZED_CONFIG_PATH="/Users/macbook/Desktop/answer_customized.txt"
 DEFAULT_CONFIG_PATH="/home/xhuang17/OpenStackonaStick/answer.txt"
+
+echo ""
 echo "CUSTOMIZED_CONFIG_PATH is " $CUSTOMIZED_CONFIG_PATH
+
+echo ""
 while read -p "Want to proceed with default config -- allinone?(yes/no) " REPLY1 ; do
   case $REPLY1 in
-    yes) echo "Installing with the default config..."
+    yes)
+    	echo ""
+    	echo "Installing with the default config..."
 	# install with allinone
 
 	break;;
-    no) echo "Checking if the customized config file is existed..."
+    no) 
+    	echo ""
+    	echo "Checking if the customized config file is existed..."
 	if [ -f $CUSTOMIZED_CONFIG_PATH ]; then
 		# the customized config file is existed
+		echo ""
 		echo "The customized config file is existed."
+		
+		echo ""
 		while read -p "Install with the existed file?(yes/no) " REPLY2 ; do
   			case $REPLY2 in
-  				yes) echo "Installing with the existed config file..."
-				USER_INPUT=0;
-				# install with the existed config file
+  				yes) 
+					echo ""
+					echo "Installing with the existed config file..."
+					USER_INPUT=0;
+					# install with the existed config file
 
-				break;;
-				no)	break;;
-				*) echo "Please enter (yes) or (no).";;
+					break;;
+				no)	
+					break;;
+				*) 
+					echo ""
+					echo "Please enter (yes) or (no).";;
 			esac
 		done
 	else
+		echo ""
 		echo "The customized config file is not existed."
 	fi
 	if [ $USER_INPUT -eq 1 ]; then
+			echo ""
 			echo "Starting user input configuration..."
 			# user input configuration
+			echo ""
 			read -p "Please enter the network node IP (x.x.x.x): " NETWORK_IP
+			echo ""
 			read -p "Please enter the conpute node IP (x.x.x.x): " COMPUTE_IP
+			echo ""
 			read -p "Please enter the controller node IP (x.x.x.x): " CONTROLLER_IP
+			echo ""
 			read -p "Please enter the storage node IP (x.x.x.x): " STORAGE_IP
+			echo ""
 			while read -p "Do you want to enable NOVA?(y/n)" NOVA_ENABLE ; do
 				case $NOVA_ENABLE in
 					y)	break;;
 					n)	break;;
-					*)	echo "Please enter y/n.";;
+					*)	
+						echo ""
+						echo "Please enter y/n.";;
 				esac
 			done
+			
+			echo ""
 			while read -p "Do you want to enable NEUTREN?(y/n)" NEUTREN_ENABLE ; do
 				case $NEUTREN_ENABLE in
 					y)	break;;
 					n)	break;;
-					*)	echo "Please enter y/n.";;
+					*)	
+						echo ""
+						echo "Please enter y/n.";;
 				esac
 			done
+			
+			echo ""
 			while read -p "Do you want to enable GLANCE?(y/n)" GLANCE_ENABLE ; do
 				case $GLANCE_ENABLE in
 					y)	break;;
 					n)	break;;
-					*)	echo "Please enter y/n.";;
+					*)	
+						echo ""
+						echo "Please enter y/n.";;
 				esac
 			done
+			
+			echo ""
 			while read -p "Do you want to enable HORIZON?(y/n)" HORIZON_ENABLE ; do
 				case $HORIZON_ENABLE in
 					y)	break;;
 					n)	break;;
-					*)	echo "Please enter y/n.";;
+					*)	
+						echo ""
+						echo "Please enter y/n.";;
 				esac
 			done
+			
+			echo ""
 			while read -p "Do you want to enable CINDER?(y/n)" CINDER_ENABLE ; do
 				case $CINDER_ENABLE in
 					y)	break;;
 					n)	break;;
-					*)	echo "Please enter y/n.";;
+					*)	
+						echo ""
+						echo "Please enter y/n.";;
 				esac
 			done
+			
+			echo ""
 			while read -p "Do you want to enable SWIFT?(y/n)" SWIFT_ENABLE ; do
 				case $SWIFT_ENABLE in
 					y)	break;;
 					n)	break;;
-					*)	echo "Please enter y/n.";;
+					*)	
+						echo ""
+						echo "Please enter y/n.";;
 				esac
 			done
 
+			echo ""
 			cp $DEFAULT_CONFIG_PATH $DEFAULT_CONFIG_PATH".bkg"
 			sed -i "s/^CONFIG_CONTROLLER_HOST.*$/CONFIG_CONTROLLER_HOST=$CONTROLLER_IP/g" $DEFAULT_CONFIG_PATH
 			sed -i "s/^CONFIG_NETWORK_HOST.*$/CONFIG_NETWORK_HOST=$NETWORK_IP/g" $DEFAULT_CONFIG_PATH
@@ -96,6 +143,8 @@ while read -p "Want to proceed with default config -- allinone?(yes/no) " REPLY1
 
 	fi
 	break;;
-    *) echo "Please enter (yes) or (no).";;
+    *) 
+    	echo ""
+    	echo "Please enter (yes) or (no).";;
   esac
 done
