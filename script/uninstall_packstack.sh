fc56d1846285de3299f51614776b08c6ed2359d8
jsingh8 <jsingh8@ncsu.edu>
2017-04-15 20:12:41 -0400

    uninstall packstack

---
+++ script/uninstall_packstack.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [ ! -f clean_up.sh ]
+        echo ""
+        echo "clean_ip.sh file is missing."
+        exit 1;
+fi
+
+if [ -f config/answer.txt ] ; then
+
+        if [ ! -z $CONFIG_COMPUTE_HOST ] ; then
+                echo ""
+                echo "Uninstalling packstack on compute node."
+
+                # scp the clean up scrip to the compute node.
+                scp scripts/clean_up.sh root@$CONFIG_CONTROLLER_HOST:/root/
+
+                # run clean up script by doing SSH to the node.
+                ssh root@$CONFIG_CONTROLLER_HOST <<ENDSSH
+
+                        source clean_up.sh
+                ENDSSH
+        fi
+
+        if [ ! -z $CONFIG_NETWORK_HOST ] ; then
+                echo ""
+                echo "Uninstalling packstack on network node."
+
+                # scp the clean up scrip to the network node.
+                scp scripts/clean_up.sh root@$CONFIG_NETWORK_HOST:/root/
+
+                # run clean up script by doing SSH to the node.
+                ssh root@$CONFIG_NETWORK_HOST <<ENDSSH
+
+                        source clean_up.sh
+                ENDSSH
+        fi
+		
+		
+        if [ ! -z $CONFIG_STORAGE_HOST ] ; then
+                echo ""
+                echo "Uninstalling packstack on storage node."
+
+                # scp the clean up scrip to the controller node.
+                scp scripts/clean_up.sh root@$CONFIG_CONTROLLER_HOST:/root/
+
+                # run clean up script by doing SSH to the node.
+                ssh root@$CONFIG_CONTROLLER_HOST <<ENDSSH
+
+                        source clean_up.sh
+                ENDSSH
+        fi
+
+        echo ""
+        echo "Uninstalling packstack on controller node."
+        source clean_up.sh
+else
+        echo ""
+        echo "Uninstalling packstack on controller node."
+        source clean_up.sh
+fi
